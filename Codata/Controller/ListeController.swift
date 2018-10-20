5c59a07d43dbb893b8409cffb2bd8384511bcb4b
Kevin Trebossen <kevintrebossen@imac-de-kevin.local>
2018-10-20 22:00:24 +0200

    Commit - Save the list in coreData

--- Codata/Controller/ListeController.swift
+++ Codata/Controller/ListeController.swift
@@ -7,21 +7,36 @@
 //
 
 import UIKit
+import CoreData
 
 class ListeController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var holderView: UIView!
     @IBOutlet weak var nameTF: UITextField!
+    
+    var listes: [Liste] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateListe()           // chargement de la liste
+    }
+    
+    func updateListe() {
+        coreDataHelper().getList { (listes) in
+            if listes != nil {
+                DispatchQueue.main.async {
+                    self.listes = listes!
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
 
     
     @IBAction func addListe(_ sender: UIButton){
-        
+        coreDataHelper().saveListe(nameTF.text)
+        updateListe()
     }
 }
 
---
+++ Codata/Model/CoreDataHelper.swift
@@ -0,0 +1,57 @@
+//
+//  CoreDataHelper.swift
+//  Codata
+//
+//  Created by Kevin Trebossen on 20/10/2018.
+//  Copyright © 2018 KTD. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+typealias ListeCompletion = (_ listes: [Liste]?) -> Void
+
+class coreDataHelper {
+    
+    // Récuprérer base CoreData
+    private let appDel = UIApplication.shared.delegate as! AppDelegate
+    
+    // Récupérer le contexte
+    var context: NSManagedObjectContext {
+        return appDel.persistentContainer.viewContext
+    }
+    
+    // Sauvegarder dans CoreData
+    func save() {                   // fonction pour sauvegarder une donnée
+        appDel.saveContext()
+    }
+    
+    
+    ////////////    Logique Entity Liste    ////////////
+    
+    // Sauvegarde de la liste
+    func saveListe(_ string: String?) {
+        guard let texte = string, texte != "" else { return }
+        let nouvelleListe = Liste(context: context)
+        nouvelleListe.date = Date()
+        nouvelleListe.name = texte
+        save()
+        print("Sauvegardé")
+    }
+    
+    func getList(completion: ListeCompletion?) {
+        let fetchRequest: NSFetchRequest<Liste> = Liste.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "date", ascending: true)             // classement de la liste (façon de trier les données
+        fetchRequest.sortDescriptors = [sortDescriptor]                                 // On donne cette façon de trier les données à la requête
+        do {
+            let listes = try context.fetch(fetchRequest)
+            for l in listes {
+                print(l.name)
+            }
+            completion?(listes)
+        } catch {
+            completion?(nil)
+            print(error.localizedDescription)
+        }
+    }
+}
